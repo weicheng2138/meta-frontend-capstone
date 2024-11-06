ec4c7a78627933573e3ddd53b6469b18a184d46a
weicheng2138 <weicheng2138@gmail.com>
2024-11-06 22:54:33 +0800

    feat(router): switch to hash router for 404 missmatch in github page

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import About from "./pages/About.jsx";
 import Error from "./pages/Error.jsx";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      errorElement: <Error />,
-    },
-    {
-      path: "/about",
-      element: <About />,
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: import.meta.env.BASE_URL,
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
   },
-);
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />

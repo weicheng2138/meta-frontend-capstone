31571a7f59e82f064dad4a250a8be92da9405f39
weicheng2138 <weicheng2138@gmail.com>
2024-11-06 22:46:40 +0800

    feat(error): clean style from vite default

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,10 @@
 function Error() {
   return (
-    <div className="h-svh w-svw flex justify-center items-center flex-col">
-      <h1>404</h1>
-      <p>Page not found</p>
+    <div className="h-svh w-svw flex justify-center items-center flex-col p-2">
+      <h1 className="text-4xl font-medium">404</h1>
+      <p className="text-gray-600 text-sm text-center">
+        🍋 Page not found and there is no Lemon here...
+      </p>
     </div>
   );
 }
